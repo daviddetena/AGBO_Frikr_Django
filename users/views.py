e3c4c96fcf9c7df4e08c3113611cd1faa5da3cce
David de Tena <daviddetenacalvo@gmail.com>
2015-07-06 21:08:57 +0200

    Usamos Django forms para el formulario de login

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@
 # Controlador para funciones de usuario
 from django.shortcuts import render, redirect
 from django.contrib.auth import login as django_login, logout as django_logout, authenticate
-
+from users.forms import LoginForm
 
 def login(request):
     """
@@ -10,27 +10,38 @@ def login(request):
     :param request:
     :return:
     """
+
     error_messages = []
     if request.method == 'POST':
-        # En Python NO acceder nunca mediante corchete a la clave de un diccionario
-        # Si usamos el .get(,) nos comprueba que exista la clave, o devuelve lo del 2º param
-        username = request.POST.get('usr')
-        password = request.POST.get('pwd')
+        # Recoge todos los datos del post y los limpia
+        form = LoginForm(request.POST)
 
-        # Encripta la contraseña automaticamente para comprobar sus credenciales. Si no existe
-        # usuario, devuelve nulo
-        user = authenticate(username=username, password=password)
-        if user is None:
-            error_messages.append('Nombre de usuario o contraseña incorrectos')
-        else:
-            if user.is_active:
-                # Activamos sesion
-                django_login(request, user)
-                return redirect('photos_home')
+        # Validamos formulario
+        if form.is_valid():
+            # En Python NO acceder nunca mediante corchete a la clave de un diccionario
+            # form.cleaned_data contendrá los datos limpiados de los campos que recojamos (que están en el /users/forms.py)
+
+            username = form.cleaned_data.get('usr')
+            password = form.cleaned_data.get('pwd')
+
+            # Encripta la contraseña automaticamente para comprobar sus credenciales. Si no existe
+            # usuario, devuelve nulo
+            user = authenticate(username=username, password=password)
+            if user is None:
+                error_messages.append('Nombre de usuario o contraseña incorrectos')
             else:
-                error_messages.append('El usuario no está activo')
+                if user.is_active:
+                    # Activamos sesion
+                    django_login(request, user)
+                    return redirect('photos_home')
+                else:
+                    error_messages.append('El usuario no está activo')
+    else:
+        # Por GET el formulario no existe
+        form = LoginForm()
     context = {
-        'errors': error_messages
+        'errors': error_messages,
+        'login_form': form
     }
     # Hacemos que el render tenga disponible el contexto con los datos que pasamos de error
     return render(request, 'users/login.html', context)
