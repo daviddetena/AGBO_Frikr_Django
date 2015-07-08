b45095ac88d51fcbb2fb0b2e60c1157472009ac2
David de Tena <daviddetenacalvo@gmail.com>
2015-07-08 20:50:21 +0200

    Creamos un endpoint de detalle de usuario

--- frikr/urls.py
+++ frikr/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from photos.views import HomeView, DetailView, CreateView, PhotoListView, UserPhotosView
 from users.views import LoginView, LogoutView
 from django.contrib.auth.decorators import  login_required
-from users.api import UserListAPI
+from users.api import UserListAPI, UserDetailAPI
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
@@ -35,5 +35,6 @@ urlpatterns = [
 
 
     # Users API URLs
-    url(r'^api/1.0/users/$', UserListAPI.as_view(), name='user_list_api')
+    url(r'^api/1.0/users/$', UserListAPI.as_view(), name='user_list_api'),
+    url(r'^api/1.0/users/(?P<pk>[0-9]+)$', UserDetailAPI.as_view(), name='user_detail_api'),
 ]
--- users/api.py
+++ users/api.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from users.serializers import UserSerializer
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from django.shortcuts import get_object_or_404
 
 class UserListAPI(APIView):
 
@@ -17,3 +18,17 @@ class UserListAPI(APIView):
         serializer = UserSerializer(users, many=True)
         serialized_users = serializer.data
         return Response(serialized_users)
+
+
+class UserDetailAPI(APIView):
+
+    def get(self, request, pk):
+        """
+        Devolvemos la vista detalle de un usuario concreto. El filtro es que el pk sea el que me pasan
+        :param request:
+        :param pk: Parámetro con el pk del usuario
+        :return: Si existe devuelve el objeto, si no devuelve un 404
+        """
+        user = get_object_or_404(User, pk=pk)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
