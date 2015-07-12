88bef6a38609169a8b5a2d3b2710b7d0fa333123
David de Tena <daviddetenacalvo@gmail.com>
2015-07-12 21:48:29 +0200

    Usando dos serializers distintos, mostramos diferentes datos en los listados y detalle de fotos

--- photos/api.py
+++ photos/api.py
@@ -1,6 +1,6 @@
 #-*- coding: utf-8 -*-
 from models import Photo
-from photos.serializers import PhotoSerializer
+from photos.serializers import PhotoSerializer, PhotoListSerializer
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 """
@@ -10,7 +10,17 @@ para que automaticamente haga el metodo POST al api
 # get, post
 class PhotoListAPI(ListCreateAPIView):
     queryset = Photo.objects.all()
-    serializer_class = PhotoSerializer
+
+    def get_serializer_class(self):
+        """
+        Sobreescribimos el atributo serializer_class para que devuelve la clase PhotoSerializer
+        en el caso de una petición POST (crear nueva foto) o nuestra clase personalizada
+        PhotoListSerializer solamente con los campos que nos interesan para el listado (id,name,url)
+        :return:
+        """
+        return PhotoSerializer if self.request.method == "POST" else PhotoListSerializer
+
+
 
 # get, put, delete
 class PhotoDetailAPI(RetrieveUpdateDestroyAPIView):
--- photos/serializers.py
+++ photos/serializers.py
@@ -6,3 +6,11 @@ class PhotoSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Photo
+
+class PhotoListSerializer(PhotoSerializer):
+    """
+    Esta clase hereda de la anterior, pero necesitamos que la clase Meta herede de la de Meta
+    de PhotoSerializer, para que asigne Photo como model
+    """
+    class Meta(PhotoSerializer.Meta):
+        fields = ('id', 'name', 'url')
